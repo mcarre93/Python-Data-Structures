1692fef487aee2c7fa689e3c08c5b21f689d765b
Madeleine Carre <mcarre93@gmail.com>
2018-04-28 21:50:07 -0400

    Added 8.4.py

---
+++ 8.4.py
@@ -0,0 +1,19 @@
+# 8.4 Open the file romeo.txt and read it line by line. For each line, split the
+# line into a list of words using the split() method. The program should build a
+# list of words. For each word on each line check to see if the word is already in
+# the list and if not append it to the list. When the program completes, sort and
+# print the resulting words in alphabetical order.
+
+wordlist = []
+finallist =[]
+fname = input("Enter file name: ")
+fh = open(fname)
+
+for line in fh:
+    wordlist = line.split()
+    for words in wordlist:
+        if words not in finallist:
+            finallist.append(words)
+
+finallist.sort()
+print(finallist)

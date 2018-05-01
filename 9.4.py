e619b492b564274b0cb75371aac63aefd403e91b
Madeleine Carre <mcarre93@gmail.com>
2018-05-01 19:27:27 -0400

    9.4.py Updated

--- 9.4.py
+++ 9.4.py
@@ -19,28 +19,22 @@ handle = open(name)
 for line in handle:
     if line.startswith("From "):
         linelist = line.split()
-        senderlist.append(linelist[1])
-        for sender in senderlist:
-                # One method of checking frequency and updating variable
-                if sender in counts:
-                    counts[sender] = counts[sender] + 1
-                else:
-                    counts[sender] =  1
+        sender = linelist[1]
+        if sender in counts:
+            counts[sender] = counts[sender] + 1
+        else:
+            counts[sender] =  1
                 # More concise method of retrieving, creating, and updating
                 # counts[sender] = counts.get(sender,0) + 1
     else:
         continue
 
-#print(counts)
-print(senderlist)
-
 # Using -1 as a flag in this case is acceptable because we are only dealing with
 # positive values in finding the highest frequency email sender
-# largest = -1
-# prolific_sender = None
-# for key,value in counts.items():
-#     #print(counts)
-#     if value > largest:
-#         largest = value
-#         prolific_sender = key
-# print(prolific_sender, largest)
+largest = -1
+prolific_sender = None
+for key,value in counts.items():
+    if value > largest:
+        largest = value
+        prolific_sender = key
+print(prolific_sender, largest)
